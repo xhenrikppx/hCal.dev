f6be120f98464baa791aed14dd68f14a3600463e
Henrik Persson <henrik@henrikpersson.com>
2015-02-09 16:13:46 -0500

    first attempt at g+ login

--- js/main.js
+++ js/main.js
@@ -1,5 +1,27 @@
-var app = angular.module('myApp', []);
+var app = angular.module('app', ['googleplus']);
 
 app.controller('CalendarController', ['$scope', function($scope) {
 	$scope.today = 'Monday';
 }]);
+
+
+app.config(['GooglePlusProvider', function(GooglePlusProvider) {
+     GooglePlusProvider.init({
+       clientId: '333246592816-u8v9b8kfoujvspfhovll4pe25tfi9n5u.apps.googleusercontent.com',
+       apiKey: 'AIzaSyAwum42gzOc-XRQc1vEtmSxWHi7TN5pPTI'
+     });
+}]);
+
+app.controller('AuthCtrl', ['$scope', 'GooglePlus', function ($scope, GooglePlus) {
+    $scope.login = function () {
+      GooglePlus.login().then(function (authResult) {
+          console.log(authResult);
+
+          GooglePlus.getUser().then(function (user) {
+              console.log(user);
+          });
+      }, function (err) {
+          console.log(err);
+      });
+    };
+}])
